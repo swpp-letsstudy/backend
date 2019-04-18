aedb894121eb90dceea4c8692b92740a37fa852a
Hyunwoo Jung <chaosphch@gmail.com>
2019-04-19 03:14:00 +0900

    Remove unnecessary codes

--- study/views.py
+++ study/views.py
@@ -1,7 +1,4 @@
-from rest_framework import generics, permissions
-from django.contrib.auth.models import User
-
-from study.models import *
+from rest_framework import generics
 from study.serializers import *
 from study.permissions import *
 
