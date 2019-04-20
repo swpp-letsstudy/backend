cbfc1c5292b68f835b264b72170a66317f6b4071
Hyunwoo Jung <chaosphch@gmail.com>
2019-04-20 20:08:01 +0900

    Fix bug : IsUser

--- study/permissions.py
+++ study/permissions.py
@@ -3,4 +3,4 @@ from rest_framework import permissions
 class IsUser(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        return request.user in obj.users
+        return True if obj.users.filter(id=request.user.id) else False
