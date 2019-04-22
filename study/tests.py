940b88722eea88cb7ecd47db4971b7f29218a782
Hyunwoo Jung <chaosphch@gmail.com>
2019-04-22 21:47:39 +0900

    Create user login test

--- study/tests.py
+++ study/tests.py
@@ -7,15 +7,15 @@ USERS_INFO = [{
     'password': '1234'
 } for i in range(3)]
 
-# class UserTestCase(APITestCase):
-#     def setUp(self):
-#         for USER_INFO in USERS_INFO:
-#             User.objects.create_user(**USER_INFO)
-#
-#     def test_login(self):
-#         for USER_INFO in USERS_INFO:
-#             response = self.client.post('/login/', USER_INFO)
-#             self.assertEqual(response.status_code, 200)
+class UserTestCase(APITestCase):
+    def setUp(self):
+        for USER_INFO in USERS_INFO:
+            User.objects.create_user(**USER_INFO)
+
+    def test_login(self):
+        for USER_INFO in USERS_INFO:
+            response = self.client.post('/login/', USER_INFO)
+            self.assertEqual(response.status_code, 200)
 
 GROUPS_INFO = [{
     'name': 'group{}'.format(i),
