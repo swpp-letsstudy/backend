f0c1d745ea9d3b805254377998faec6c9258e51c
JangWony <jwon2@snu.ac.kr>
2019-05-11 16:59:25 +0900

    Init testcode about AttendancesTest

--- study/tests.py
+++ study/tests.py
@@ -110,3 +110,66 @@ class MeetingTestCase(APITestCase):
 
         response = self.get('/study_meetings/?groupId={}'.format(group_id))
         self.assertEqual(response.status_code, 200)
+
+
+class AttendanceTest(APITestCase):
+
+    def setUp(self):
+        for USER_INFO in USERS_INFO:
+            User.objects.create_user(**USER_INFO)
+        self.token = None
+        self.create_meeting()
+
+    def login(self, user_info):
+        response = self.client.post('/login/', user_info)
+        self.token = response.data['token']
+        return User.objects.get(auth_token=self.token)
+
+    def header(self):
+        return {'HTTP_AUTHORIZATION': 'Token {}'.format(self.token)} if self.token else {}
+
+    def get(self, path):
+        return self.client.get(path, **self.header())
+
+    def post(self, path, data):
+        return self.client.post(path, data, **self.header())
+    
+    def put(self, path):
+        return self.client.put(path, **self.header())
+
+    def create_group(self, group_info):
+        self.post('/study_groups/', group_info)
+        return self.get('/study_groups/')
+    
+    def create_meeting(self):
+        self.login(USERS_INFO[0])
+        for GROUP_INFO in GROUPS_INFO:
+            self.create_group(GROUP_INFO)
+
+        group_id = StudyGroup.objects.all()[0].id
+
+        for MEETING_INFO in MEETINGS_INFO:
+            data = dict(list(MEETING_INFO.items()) + [('groupId',group_id)])
+            response = self.post('/study_meetings/', data)
+
+    def test_create_attendance(self):
+        
+        user_id = User.objects.all()
+        meeting_id = User.objects.all()
+
+        for user in user_id:
+            for meeting in meeting_id:
+                data = dict([('userId',user.id)]+[('meetingId',meeting.id)])
+                response = self.post('/attendances/', data)
+                self.assertEqual(response.status_code, 201)
+    '''
+        for user in user_id:
+            for meeting in meeting_id:
+                response = self.get('/attendances?userId={}&meetingId={}/'.format(user.id, meeting.id))
+                self.assertEqual(response.status_code, 301)
+
+        for user in user_id:
+            for meeting in meeting_id:
+                response = self.put('/attendances?userId={}&meetingId={}/'.format(user.id, meeting.id))
+                self.assertEqual(response.status_code, 301)
+    '''
