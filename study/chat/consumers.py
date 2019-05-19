6af5bd4509a6cf773ff7c57b1110acfe8fe3855f
Hyunwoo Jung <chaosphch@gmail.com>
2019-05-19 22:01:40 +0900

    Implement simple websocket handling codes

--- study/chat/consumers.py
+++ study/chat/consumers.py
@@ -1,5 +1,22 @@
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.contrib.auth.models import User
+import json
+
+from study.models import StudyGroup, Message
+
 
 class ChatConsumer(WebsocketConsumer):
-    pass
+
+    def connect(self):
+        self.accept()
+
+    def disconnect(self, code):
+        pass
+
+    def receive(self, text_data=None, bytes_data=None):
+        data = json.loads(text_data)
+        user = User.objects.filter(id=data['userId'])[0]
+        study_group = StudyGroup.objects.filter(id=data['groupId'])[0]
+        print(user, study_group)
+        Message.objects.create(user=user, study_group=study_group, content=data['content'])
