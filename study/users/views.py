74391c7f32ca6ec326b0de626940082b165158f1
JeonChangMin <wisechang1@naver.com>
2019-05-28 16:52:12 +0900

    Nickname

--- study/users/views.py
+++ study/users/views.py
@@ -26,7 +26,7 @@ class MyLoginView(ObtainAuthToken): # login/
 class MyRegisterView(APIView): # register/
     # POST { username, password }, register
     def post(self, request, format=None):
-        if 'username' in request.data.keys() and 'password' in request.data.keys():
+        if 'username' in request.data.keys() and 'password' in request.data.keys() and 'nickname' in request.data.keys():
             if User.objects.filter(username=request.data['username']).exists():
                 return Response('username already exists', status=409)
                 
@@ -34,10 +34,9 @@ class MyRegisterView(APIView): # register/
                 return Response('nickname already exists', status=409)
 
             user = User.objects.create_user(username=self.request.data['username'], password=self.request.data['password'])
-            if 'nickname' in request.data.keys():
-                studyuser = StudyUser.objects.get(user=user)
-                studyuser.nickname = request.data['nickname']
-                studyuser.save()
+            studyuser = StudyUser.objects.get(user=user)
+            studyuser.nickname = request.data['nickname']
+            studyuser.save()
                 
             return Response('successed', status=201)
         else:
